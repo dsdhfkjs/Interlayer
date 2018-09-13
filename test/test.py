1351d0988c3dd166c03c0deb8cbe73572359c13d
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-09-13 23:48:52 +0900

    Remove test.py

--- test/test.py
+++
@@ -1,7 +0,0 @@
-layer_Al4 = Interlayer(v2p.vasp2pymatgen("test/vasp/POSCAR_Al4"))
-layer_NaCl = Interlayer(v2p.vasp2pymatgen("test/vasp/POSCAR_NaCl"))
-layer_Si = Interlayer(v2p.vasp2pymatgen("test/vasp/POSCAR_Si"))
-
-layer_Al4.show_irreducible_list()
-layer_NaCl.show_irreducible_list()
-layer_Si.show_irreducible_list()
