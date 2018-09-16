6a067f1acb58b5476a65cd466fc9ce1ec9142604
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-09-16 23:28:20 +0900

    Remove import test module

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import src.Interlayer
 from src.Interlayer import Interlayer
 import src.Vasp2pymatgen
 from src.Vasp2pymatgen import Vasp2pymatgen as v2p
-import test
-import test.test as te
 
 if __name__ == "__main__":
     layer_Al4 = Interlayer(v2p.vasp2pymatgen("test/vasp/POSCAR_Al4"))
