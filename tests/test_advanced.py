715d190b1327b803db762aeeb25173d13a24fa2b
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-09-20 23:18:57 +0900

    Create tests to practice

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from .context import src
 
 import unittest
 
 
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
-
-    def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
-
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from .context import src
+from src.Interlayer import Interlayer
+from src.Vasp2pymatgen import Vasp2pymatgen as v2p
 
 import unittest
 
@@ -10,6 +12,15 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_absolute_truth_and_meaning(self):
         assert True
+    
+    def test_basic(self):
+        layer_Al4 = Interlayer(v2p.vasp2pymatgen("tests/vasp/POSCAR_Al4"))
+        layer_NaCl = Interlayer(v2p.vasp2pymatgen("tests/vasp/POSCAR_NaCl"))
+        layer_Si = Interlayer(v2p.vasp2pymatgen("tests/vasp/POSCAR_Si"))
+        
+        layer_Al4.show_irreducible_list()
+        layer_NaCl.show_irreducible_list()
+        layer_Si.show_irreducible_list()
 
 
 if __name__ == '__main__':
